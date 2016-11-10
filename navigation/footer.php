0242b98c9ba5ef110360a1441a0f692d7c4e3e47
pratekmag <prateekmag@cybage.com>
2016-11-10 23:49:18 +0530

    wefewf

--- navigation/footer.php
+++ navigation/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    Footer
+    <span>Copyright &copy; 2016 EATEAM INC All rights reserved</span>
 </footer>
 </body>
 </html>
---
+++ navigation/site-links.php
@@ -0,0 +1,56 @@
+
+<div class="site-links">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3">
+				<h4>CORPORATE</h4>
+				<ul>
+					<li><a href="#">About Us</a></li>
+					<li><a href="#">Eateam strategy</a></li>
+					<li><a href="#">Mission Statement</a></li>
+					<li><a href="#">Advantage</a></li>
+					<li><a href="#">Management Team</a></li>
+					<li><a href="#">Our Clients</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-3">
+				<h4>SERVICES</h4>
+				<ul>
+					<li><a href="#">IT Services</a></li>
+					<li><a href="#">Business Consulting</a></li>
+					<li><a href="#">Application Development</a></li>
+					<li><a href="#">Application Maintenence</a></li>
+					<li><a href="#">Testing / Quality Assurence</a></li>
+					<li><a href="#">Software Development</a></li>
+					<li><a href="#">ERP Service and Solutions</a></li>
+					<li><a href="#">NFC Solutions</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-3">
+				<h4>TALENT</h4>
+				<ul>
+					<li><a href="#">Current Openings</a></li>
+					<li><a href="#">Contract Jobs</a></li>
+					<li><a href="#">Benefits</a></li>
+					<li><a href="#">EOE</a></li>
+					<li><a href="#">E-verify</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-3">
+				<h4>CONTACT</h4>
+				<ul>
+					<li>2 Kilmer Rd, Edison NJ 08817</li>
+					<li>Phone: 732-339-0024</li>
+					<li>Fax: 732-339-0039</li>
+					<li>Email: info@eateam.com</li>
+					<li class="social-icons">
+						<a href="#"><i class="icon-facebook"></i></a>
+						<a href="#"><i class="icon-tweeter"></i></a>
+						<a href="#"><i class="icon-in"></i></a>
+					</li>
+				</address>
+			</div>
+
+		</div>
+	</div>
+</div>
